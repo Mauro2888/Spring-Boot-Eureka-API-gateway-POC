84d7d4dd6ac414aa48dbca797700f98250287e07
Mauro Caredda <mauro.dev88@gmail.com>
2022-01-17 11:09:28 +0100

    Update AuthenticationFilter.java

--- UserService/src/main/java/com/user/service/userservice/security/AuthenticationFilter.java
+++ UserService/src/main/java/com/user/service/userservice/security/AuthenticationFilter.java
@@ -1,20 +1,42 @@
 package com.user.service.userservice.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.user.service.userservice.dto.UserDto;
 import com.user.service.userservice.model.LoginRequestModel;
+import com.user.service.userservice.service.UsersService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UsersService usersService;
+    private Environment environment;
+
+    public AuthenticationFilter(UsersService usersService, Environment environment, AuthenticationManager authenticationManager) {
+        this.usersService = usersService;
+        this.environment = environment;
+        super.setAuthenticationManager(authenticationManager);
+    }
+
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response) throws AuthenticationException {
@@ -27,7 +49,8 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                     new UsernamePasswordAuthenticationToken(
                             creds.getEmail(),
                             creds.getPassword(),
-                            new ArrayList<>()));
+                            new ArrayList<>())
+            );
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -38,5 +61,27 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
+
+        String userName = ((User) authResult.getPrincipal()).getUsername();
+        UserDto userDetails = usersService.getUserDetailsByEmail(userName);
+
+        //GENERATE TOKEN
+        String token = Jwts.builder()
+                .setSubject(userDetails.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(environment.getProperty("token.expiration_time"))))
+                .signWith(SignatureAlgorithm.HS512, environment.getProperty("token.secret"))
+                .compact();
+
+        response.addHeader("token", token);
+        response.addHeader("userId", userDetails.getUserId());
+    }
+
+    private String generateSafeToken() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[36]; // 36 bytes * 8 = 288 bits, a little bit more than
+        // the 256 required bits
+        random.nextBytes(bytes);
+        var encoder = Base64.getUrlEncoder().withoutPadding();
+        return encoder.encodeToString(bytes);
     }
 }
