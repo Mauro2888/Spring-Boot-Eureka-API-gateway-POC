a83c7965bb4f68787b5446ccbc05f3f335c55045
Mauro Caredda <mauro.dev88@gmail.com>
2022-01-17 11:10:52 +0100

    Update UserController.java

--- UserService/src/main/java/com/user/service/userservice/controller/UserController.java
+++ UserService/src/main/java/com/user/service/userservice/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.user.service.userservice.controller;
 
-import com.user.service.userservice.data.UserEntity;
 import com.user.service.userservice.dto.UserDto;
 import com.user.service.userservice.model.CreateUserRequestModel;
 import com.user.service.userservice.model.CreateUserResponseModel;
@@ -8,12 +7,13 @@ import com.user.service.userservice.service.UsersService;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
 
@@ -22,7 +22,6 @@ import javax.validation.Valid;
 public class UserController {
 
     private final UsersService usersService;
-
     private final Environment environment;
 
     @Autowired
@@ -31,14 +30,15 @@ public class UserController {
         this.environment = environment;
     }
 
+
     @GetMapping("/check")
     public String checkAccount(){
-        return "Working user on port" + environment.getProperty("local.server.port");
+        return "Working user on port " + environment.getProperty("local.server.port");
     }
 
-
-    @PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE},
-                produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE})
+    /*consumes = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE},
+    produces = {MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_JSON_VALUE}*/
+    @PostMapping()
     public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetail){
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
